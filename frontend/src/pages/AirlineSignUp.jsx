ccf8ad9c7fff19aa55637a3cb1e9ece9d4633b99
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2025-11-22 13:47:31 -0500

    Refactor AirlineSignUp component and update form fields

--- frontend/src/pages/AirlineSignUp.jsx
+++ frontend/src/pages/AirlineSignUp.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"; 
-import { Link } from "react-router-dom"; //Used for navigation between login and signup
+import { useEffect, useState } from "react"; 
+import { Link, useNavigate } from "react-router-dom"; //Used for navigation between login and signup
 import axios from "axios"; //HTTP client for making requests/API calls to backend
 import "./UserInfoPage.css";
+import toast from "react-hot-toast";
 
 const AirlineSignup = () => {
   // State to toggle password visibility
@@ -12,11 +13,18 @@ const AirlineSignup = () => {
 
   // State to hold form input data
   const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
+    firstName: '',
+    lastName: '',
     password: '',
+    companyName: '',
+    country: '',
+    phoneNumber: '',
+    companyWebsite: '',
+    contactEmail: '',
+    role: "seller",
   });
+
+  const navigate = useNavigate();
 
     // Function to update formData when input changes
     const handleChange = (e) => { 
@@ -24,6 +32,10 @@ const AirlineSignup = () => {
       // Spread the old values, update only the one that changed
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  useEffect(()=> {
+    console.log(formData);
+  },[formData]);
 
 
   // Function to handle form submission
@@ -33,14 +45,13 @@ const AirlineSignup = () => {
     try{
       // Make POST request to backend API to register user
       console.log(formData);
-        const res = await axios.post('http://localhost:5000/api/users/register', formData);
+        const res = await axios.post('http://localhost:5000/api/sellers/register', formData);
         
-        alert("Form submitted!"); // Confirmation for user
-        console.log("Saved user: ", res.data); // Log the response from backend
+        toast.success("Form submitted!"); // Confirmation for user
+        navigate("/airline");
     }catch(err){
       // Catch and log any errors
-        console.error("Error saving user: ", err);
-        alert("Error submitting form");
+        toast.error("Error submitting form");
     }
   };
   
@@ -54,7 +65,7 @@ const AirlineSignup = () => {
                 <div className="form-group">
                     <input 
                     type="text"
-                    name="firstname"
+                    name="firstName"
                     placeholder="First Name"
                     value={formData.firstname}
                     onChange={handleChange}
@@ -65,7 +76,7 @@ const AirlineSignup = () => {
                 <div className="form-group">
                     <input 
                     type="text"
-                    name="lastname"
+                    name="lastName"
                     placeholder="Last Name"
                     value={formData.lastname}
                     onChange={handleChange}
@@ -76,9 +87,9 @@ const AirlineSignup = () => {
                 <div className="form-group">
                     <input 
                     type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={formData.email}
+                    name="contactEmail"
+                    placeholder="Contact Email"
+                    value={formData.contactEmail}
                     onChange={handleChange}
                     required
                     />
@@ -96,9 +107,55 @@ const AirlineSignup = () => {
                 </div>
 
                 <div className="form-group">
+                    <input 
+                    type="text"
+                    name="companyName"
+                    placeholder="company Name"
+                    value={formData.companyName}
+                    onChange={handleChange}
+                    required
+                    />
+                </div>
+
+
+                <div className="form-group">
+                    <input 
+                    type="text"
+                    name="country"
+                    placeholder="Country"
+                    value={formData.country}
+                    onChange={handleChange}
+                    required
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input 
+                    type="text"
+                    name="phoneNumber"
+                    placeholder="phone Number"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    required
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input 
+                    type="text"
+                    name="companyWebsite"
+                    placeholder="Company Website"
+                    value={formData.companyWebsite}
+                    onChange={handleChange}
+                    required
+                    />
+                </div>
+
+                <div className="form-group">
                     <input type="checkbox" id="showPassword" onChange={togglePassword} />
                     <label htmlFor="showPassword">Show Password</label>
                 </div>
+                
 
                 <button type="submit" className="submit-button">Sign Up</button>
                 <p>Already have an account? <Link to="/Login">Log in here</Link></p>
