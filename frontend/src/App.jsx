83e85dbc36e1f543a5e59700678868bb2a2b8731
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2025-11-30 14:42:09 -0500

    Add role-based routing for admin and seller

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,24 +13,35 @@ import AirlineSignup from "./pages/AirlineSignUp";
 import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
 import FlightDetail from "./components/FlightDetail";
-
+import PaymentPage from "./pages/PaymentPage";
+import Admin from "./pages/Admin";
+import UserContext from "./UserContext";
+import { useContext, useState } from "react";
+import Unauthorized from "./pages/Unauthorized";
+import MyFlights from "./pages/MyFlights";
 
 function App() {
+  const { role } = useContext(UserContext);
+  
   return (
     <div>
-    
+    <Toaster />
     {/* // Router wraps the entire application to enable routing */}
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/note/:id" element={<FlightDetail />} />
+        <Route path="/admin" element={role == "admin" ? <Admin /> : <Unauthorized />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/flight/:id" element={<FlightDetail />} />
         <Route path="/airlineSignup" element={<AirlineSignup />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/addflight" element={<AddFlight />} />
-        <Route path="/infopage" element={<UserInfoPage />} />
+        <Route path="/addflight" element={role === "seller" || role === "admin" ? <AddFlight /> : <Unauthorized />} />
+        <Route path="myflights" element={role === "seller" || role === "admin" ? <MyFlights /> : <Unauthorized />} />
+        <Route path="/infopage" element={role === "passenger" ? <UserInfoPage /> : <Unauthorized />} />
         <Route path="/support" element={<Support />} />
-        <Route path="/airline" element={<Airline />} />
+        <Route path="/airline" element={role === "seller" || role === "admin" ? <Airline /> : <Unauthorized />} />
         <Route path="/settings" element={<Settings />} />
 
       </Routes>
